e4cd656d1f8f2c8ed136f49faaadf1d4179776aa
mads94i5 <mads94i5@stud.kea.dk>
2023-04-19 16:14:18 +0200

    game details

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,23 +1,17 @@
-import {sanitizeStringWithList} from "../../utils/utils.js"
 import { API_URL } from "../../utils/settings.js";
 import { fetchGetJson } from "../../utils/utils.js"
+import { sanitizeStringWithList } from "../../utils/utils.js"
 
   
   export async function initHome(){
-    console.log("initiated");
   document.getElementById("generate").addEventListener("click", async function (event) {
     const game = await fetchGetJson(API_URL + "gameidea/create");
     console.log(game)
     const listGame = 
-        `  <h2>${game.title}</h2>
-        <ul>
-          <li><strong>ID:</strong> ${game.id}</li>
-          <li><strong>Description:</strong> ${game.description}</li>
-          <li><strong>Genre:</strong> ${game.genre}</li>
-          <li><strong>Number of players:</strong> ${game.player}</li>
-        </ul>`
-        
-
+        `<h2>${game.title}</h2>
+          <strong>Description:</strong> ${game.description} <br>
+          <strong>Genre:</strong> ${game.genre} <br>
+          <strong>You play as:</strong> ${game.player}`
       const okList = sanitizeStringWithList(listGame)
       document.getElementById("string-list").innerHTML = okList
     
