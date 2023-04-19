30fa1dc10726e23006852ee73235c19b0020c401
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-04-19 16:26:01 +0200

    spinner

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,24 +1,17 @@
-<<<<<<< HEAD
 import {sanitizeStringWithParagraph} from "../../utils/utils.js"
-=======
->>>>>>> e4cd656d1f8f2c8ed136f49faaadf1d4179776aa
 import { API_URL } from "../../utils/settings.js";
 import { fetchGetJson } from "../../utils/utils.js"
-import { sanitizeStringWithList } from "../../utils/utils.js"
 
   
   export async function initHome(){
-<<<<<<< HEAD
 
     console.log("initiated");
 
     const spinner = document.getElementById("spinner");
     const stringList = document.getElementById("string-list");
-
+    
     spinner.style.display = "none";
 
-=======
->>>>>>> e4cd656d1f8f2c8ed136f49faaadf1d4179776aa
   document.getElementById("generate").addEventListener("click", async function (event) {
     spinner.style.display = "block";
     stringList.style.display = "none";
@@ -28,7 +21,6 @@ import { sanitizeStringWithList } from "../../utils/utils.js"
     console.log(game)
 
     const listGame = 
-<<<<<<< HEAD
         `  <h2>${game.title}</h2>
           <p><strong>Description:</strong> ${game.description}</p>
           <p><strong>Genre:</strong> ${game.genre}</p>
@@ -42,14 +34,6 @@ import { sanitizeStringWithList } from "../../utils/utils.js"
 
       spinner.style.display = "none";
       stringList.style.display = "block";
-=======
-        `<h2>${game.title}</h2>
-          <strong>Description:</strong> ${game.description} <br>
-          <strong>Genre:</strong> ${game.genre} <br>
-          <strong>You play as:</strong> ${game.player}`
-      const okList = sanitizeStringWithList(listGame)
-      document.getElementById("string-list").innerHTML = okList
->>>>>>> e4cd656d1f8f2c8ed136f49faaadf1d4179776aa
     
   })
 }
