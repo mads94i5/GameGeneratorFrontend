fce5e581a30fb252722abf27182246b7a3ff6d7d
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-04-19 21:58:37 +0200

    minor changes in index.js home

--- pages/home/index.js
+++ pages/home/index.js
@@ -3,6 +3,7 @@ import { API_URL } from "../../utils/settings.js";
 import { fetchGetJson } from "../../utils/utils.js";
 
 export async function initHome() {
+  
   const spinner = document.getElementById("spinner");
   const stringList = document.getElementById("string-list");
 
@@ -18,13 +19,13 @@ export async function initHome() {
       
 
       console.log(game);
-      //Gameimage needs to be buffered into image
+      //Gameimage needs to be buffered into image ${game.image}
       const listGame = `<hr><h2>${game.title}</h2>
     <p style="font-size: 0.8em;text-align: left;margin-left: 2em;margin-right: 2em;">
     <strong>Description:</strong> ${game.description} <br>
     <strong>Genre:</strong> ${game.genre} <br>
     <strong>You play as:</strong> ${game.player} <br>
-    <strong>Image:</strong> <div>${game.image}</div> 
+    <strong>Image:</strong> <div></div> 
     <p><a href="/gamedetails/${game.id}"><button class="btn-success">Game info</button></a></p>
     </p><br>`;
 
