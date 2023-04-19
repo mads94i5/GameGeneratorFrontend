0846c83b31d19caab2c3cc52f95b01ccb1471ed7
ThomasSA-Git <thom45w9@stud.kea.dk>
2023-04-19 12:51:34 +0200

    fetch image works

--- pages/gamedetails/index.js
+++ pages/gamedetails/index.js
@@ -1,3 +1,16 @@
-export function initGameDetails(){
-  
+import { fetchGetImage } from "../../utils/utils.js"
+import { API_URL } from "../../utils/settings.js";
+
+
+export async function initGameDetails(){
+
+    console.log("initiated")
+
+
+const prompt = 'cover for a pc game where the player type is a goat and the genre is rpg';
+const url = API_URL + `imagegenerator/` + prompt;
+
+fetchGetImage(url);
+
+
 }
