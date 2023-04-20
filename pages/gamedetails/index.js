2b4c4431b67aec4624eeeafc85c535b6f44749c2
mads94i5 <mads94i5@stud.kea.dk>
2023-04-20 19:01:27 +0200

    santize

--- pages/gamedetails/index.js
+++ pages/gamedetails/index.js
@@ -1,16 +1,15 @@
 import { API_URL } from "../../utils/settings.js";
 import { fetchGetJson } from "../../utils/utils.js"
-import { sanitizeStringWithList } from "../../utils/utils.js"
+import { sanitizeStringWithParagraph } from "../../utils/utils.js"
 
 
 export async function initGameDetails(id){
     const game = await fetchGetJson(API_URL + `gameidea/get/${id}`);
-
-    const dataUrl = "data:image/png;base64," + game.image;
+    const imageUrl = "data:image/png;base64," + game.image;
     // Build primary game HTML
     let gameHtml = `
         <h2>${game.title}</h2>
-        <img src="${dataUrl}" style="width: 512px; height: 512px;"> 
+        <img src="${imageUrl}" style="width: 512px; height: 512px;"> 
         <p style="font-size: 0.8em;text-align: left;margin-left: 2em;margin-right: 2em;">
         <strong>Description:</strong> ${game.description} <br>
         <strong>Genre:</strong> ${game.genre} <br>
@@ -33,6 +32,6 @@ export async function initGameDetails(id){
         }
     }
 
-    const okList = sanitizeStringWithList(gameHtml);
+    const okList = sanitizeStringWithParagraph(gameHtml);
     document.getElementById("string-list").innerHTML = okList;
 }
