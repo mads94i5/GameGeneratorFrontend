3f58d378eb51bfde3a358a5839599504a66426fa
mads94i5 <mads94i5@stud.kea.dk>
2023-04-21 10:11:00 +0200

    hosted api

--- utils/settings.js
+++ utils/settings.js
@@ -1,4 +1,4 @@
-// export const API_URL = "https://gameideagenerator.azurewebsites.net/api/"
-export const API_URL = "http://localhost:8080/api/"
+export const API_URL = "https://gameideagenerator.azurewebsites.net/api/"
+// export const API_URL = "http://localhost:8080/api/"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or does the endpoint exist?)" 
