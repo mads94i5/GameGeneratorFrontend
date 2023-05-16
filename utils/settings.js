bf3cba1a6aefde4d25b2ef092cd532ac8fd76395
mads94i5 <mads94i5@stud.kea.dk>
2023-05-16 21:04:28 +0200

    backend url

--- utils/settings.js
+++ utils/settings.js
@@ -1,10 +1,8 @@
 // Check if the API is running locally or on Azure and set the API_URL accordingly
 export const HOST_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") ?
     "http://localhost:8080/" :
-    "https://gameideagenerator.azurewebsites.net/"
+    "https://gamegeneratorbackend.azurewebsites.net/"
 
-export const API_URL = (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") ?
-    HOST_URL + "api/" :
-    HOST_URL + "api/"
+export const API_URL = HOST_URL + "api/"
 
 export const FETCH_NO_API_ERROR = " (Is the API online or does the endpoint exist?)" 
